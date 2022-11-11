99a4c48b486cc8037a8e44f463c885683362da25
Womp Rat <devinmenzies@gmail.com>
2022-11-11 16:36:17 -0700

    add pollution data from openweather api

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+// "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 const request = require("request");
+
+
 require("dotenv").config();
+
+//variables linked to dotenv file
+const apiKey = process.env.API_KEY;
+const user = process.env.USER;
+const password = process.env.PASS
+const iqAirWidget = process.env.IQAIR_WIDGET;
+const openWeatherAPI = process.env.OPEN_WEATHER_API;
 
 const app = express();
 
@@ -17,11 +27,9 @@ app.use(express.static("public"));
 
 let transporter = nodemailer.createTransport({
   host: "smtp.office365.com",
-  // port: 587,
-  // secure: false,
   auth: {
-    user: "denvairQualityMonitor@outlook.com",
-    pass: "andNoSmogFourUs"
+    user: user,
+    pass: password
   }
 });
 
@@ -32,10 +40,10 @@ let sendEmails = function(){
   ];
 
   let options = {
-    from: "denvairQualityMonitor@outlook.com",
+    from: user,
     to: mailList,
     subject: "Denver Air Quality Has Exceeded 100 AQI",
-    text: "text body",
+    text: "text body, lorem ipsum etc, etc",
   }
 
 
@@ -48,9 +56,6 @@ let sendEmails = function(){
     }
   });
 };
-
-//setting up environment variables using dotenv node module
-const apiKey = process.env.API_KEY;
 
 let options = {
   method: "GET",
@@ -73,10 +78,52 @@ request(options, function(err, response) {
   //if aqius exceeds threshold, trigger the sendEmails() function, which carries out sending the emails using nodemailer to all recipients
   if (aqius > 100){
     sendEmails();
+    console.log(`Emails sent! AQI is above the threshold, currently at ${aqius}`)
   } else {
-    console.log("Aqi too low");
+    console.log(`Emails not sent, AQI is below threshold, currently at ${aqius}`);
   }
 });
+
+let openWeatherOptions = {
+  method: "GET",
+  url: `http://api.openweathermap.org/data/2.5/air_pollution?lat=39.742043&lon=-104.991531&appid=${openWeatherAPI}`
+}
+
+let pollutantData;
+
+let getPollutantData = function(pollutantData){
+  app.get("/", function(req, res) {
+    res.render("index", {
+      iqAirWidgetKey: iqAirWidget,
+      co: pollutantData[0],
+      no: pollutantData[1],
+      no2: pollutantData[2],
+      o3: pollutantData[3],
+      so2: pollutantData[4],
+      nh3: pollutantData[5],
+      pm25: pollutantData[6],
+      pm10: pollutantData[7]
+    });
+  });
+}
+
+
+request(openWeatherOptions, function(err, response){
+  if(err) throw new Error(err);
+  let openWeatherData = JSON.parse(response.body);
+  let pollutants = openWeatherData.list[0].components;
+  pollutantData = Object.values(pollutants);
+  getPollutantData(pollutantData);
+});
+
+
+
+
+
+
+
+
+
 
 mongoose.connect("mongodb://localhost:27017/citiesDB", {
   useNewURLParser: true
@@ -88,9 +135,7 @@ const citiesSchema = {
 
 const City = mongoose.model("city", citiesSchema);
 
-app.get("/", function(req, res) {
-  res.render("index");
-});
+
 
 app.post("/", function(req, res) {
   const userCity = req.body.cityInput;
